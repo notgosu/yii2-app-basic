7c42c1a61203f4cf511119ee6d2bee5af28248b2
Oleg Kostiushko <o.kostyshko@gmail.com>
2016-07-13 21:42:53 +0300

    added test for empty contact form submit

--- tests/codeception/acceptance/ContactCest.php
+++ tests/codeception/acceptance/ContactCest.php
@@ -5,10 +5,28 @@
  */
 class ContactCest
 {
+    public function _before(\AcceptanceTester $I)
+    {
+        $I->amOnPage('site/contact');
+    }
+    
     public function openContactPage(AcceptanceTester $I)
     {
         $I->wantTo('ensure that contact page works');
-        $I->amOnPage('site/contact');
         $I->see('Contact', 'h1');
+    }
+    
+    public function submitEmptyContactForm(AcceptanceTester $I)
+    {
+        $I->wantTo('submit contact form with no data');
+        $I->click('contact-button');
+
+        $I->expectTo('see validations errors');
+        $I->see('Contact', 'h1');
+        $I->see('Name cannot be blank', '.help-block-error');
+        $I->see('Email cannot be blank', '.help-block-error');
+        $I->see('Subject cannot be blank', '.help-block-error');
+        $I->see('Body cannot be blank', '.help-block-error');
+        $I->see('The verification code is incorrect', '.help-block-error');
     }
 }
