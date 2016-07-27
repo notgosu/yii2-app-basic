b89aa1484f5990f208c753990a553443ccfcd652
Davert <davert.php@resend.cc>
2016-07-28 02:30:24 +0300

    fix @webroot in tests to point to tests/_output

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,5 +2,8 @@
 define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
+
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require __DIR__ .'/../vendor/autoload.php';
+
+Yii::setAlias('@webroot', codecept_output_dir());
